9613e5e756e97fd2e33c307d2ef861641061daf4
eullaguari <eullagua@espol.edu.ec>
2021-02-10 22:47:42 -0500

    credenciales de produccion db

--- Aceria/src/database.js
+++ Aceria/src/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const {HOST, DATABASE} = process.env;
 
-const URI = `mongodb://${HOST}:27017/${DATABASE}`; 
-
+const URI = `mongodb+srv://userMongo:juan123456@cluster0.hc1lx.mongodb.net/dbMongo?retryWrites=true&w=majority`; 
+//mongodb+srv://userMongo:<password>@cluster0.hc1lx.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
